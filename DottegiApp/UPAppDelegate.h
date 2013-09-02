08a8a4b02961a36c4dcafa38d9a00ab4e7606e60
unionpool_yangsaerohoon <yangsaerohoon@unionpool.com>
2013-09-02 10:07:31 +0900

    Initial Commit

---
+++ DottegiApp/UPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  UPAppDelegate.h
+//  DottegiApp
+//
+//  Created by Saerohoon Yang on 13. 9. 2..
+//  Copyright (c) 2013년 Union Pool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DottegiApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DottegiApp
+//
+//  Created by Saerohoon Yang on 13. 9. 2..
+//  Copyright (c) 2013년 Union Pool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UPAppDelegate class]));
+    }
+}
